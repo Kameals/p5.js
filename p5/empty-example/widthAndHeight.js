a0fd5d028cecd7550b3b4395bcfccb77b8223730
Kameals <truekhm@gmail.com>
2018-09-09 13:28:14 +0900

    seeing the logic

--- p5/empty-example/widthAndHeight.js
+++ p5/empty-example/widthAndHeight.js
@@ -4,11 +4,16 @@ function setup() {
   
   function draw() {
     background(127);
-    noStroke();
+    //noStroke();
     for (var i = 0; i < height; i += 20) {
       fill(129, 206, 15);
       rect(0, i, width, 10);
       fill(255);
       rect(i, 0, 10, height);
+      /*
+      if (i === 40) {
+          exit();
+      }
+      */
     }
   }
